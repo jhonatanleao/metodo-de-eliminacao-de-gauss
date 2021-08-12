2bc5311f4533d832553b941164ee768cb7ca03fe
eloy-freitas <freitaseloy.1@gmail.com>
2021-08-12 20:55:16 -0300

    concluindo codigo de construçao da tabela de somatorios

--- ajuste_curva/ajuste_curva.py
+++ ajuste_curva/ajuste_curva.py
@@ -16,7 +16,7 @@ def read_file(path):
         input = np.matrix(
                     np.loadtxt(
                         fname=path,
-                        dtype=np.float64,
+                        dtype=str,
                         delimiter=',',
                         max_rows=N, 
                         skiprows=1,
@@ -24,7 +24,7 @@ def read_file(path):
                     )
                 )
         X, Y = input[:, 0], input[:, 1]
-        return N, P, X, Y
+        return N, P, X.astype(np.float64), Y.astype(np.float64)
     else:
         sys.exit("Tamanho de entrada inválido!")
     
@@ -32,26 +32,30 @@ def read_file(path):
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         read_file(sys.argv[1])
-        N, P,X, Y = read_file(sys.argv[1])
+        N, P, X, Y = read_file(sys.argv[1])
         #print(f'vetor X:\n {X}')
         #print(f'vetor Y:\n {Y}')
-        matrix = Y
-        matrix = np.insert(arr=matrix, obj=1, values=X.T, axis=1)
+        
+        matrix_values = Y
+        matrix_values = np.insert(arr=matrix_values, obj=1, values=X.T, axis=1)
         for i in np.arange(2, ((2 * P) + P + 1)):
             if i <= (2 * P):
-                matrix = np.insert(
-                    arr=matrix, 
+                matrix_values = np.insert(
+                    arr=matrix_values, 
                     obj=i, 
                     values=np.power(X, i).T, 
                     axis=1)
             else:
-                matrix = np.insert(
-                    arr=matrix, 
+                matrix_values = np.insert(
+                    arr=matrix_values, 
                     obj=i, 
-                    values=np.multiply(matrix[:, i-(2*P)], matrix[:, 0]).T, 
+                    values=np.multiply(matrix_values[:, i-(2*P)], matrix_values[:, 0]).T, 
                     axis=1)
         
-        print(matrix)
-        
+        matrix_sums = np.zeros(shape=(2 * P) + P + 1)
+        for i in np.arange(0, ((2 * P) + P + 1)):
+            matrix_sums[i] = np.sum(a=matrix_values[:, i], dtype=np.float64)
+        print(matrix_values)
+        print(matrix_sums)
 
        
